230dc5d39005a22265ef7a1b87a0d8dedd134bf4
Gabrielli <gabrielli.osc@gmail.com>
2024-12-15 23:58:43 -0300

    Create SplashViewController

---
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SplashViewController.swift
+//  Reminder
+//
+//  Created by Gabi on 15/12/24.
+//
+
+import Foundation
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    let contentView = SplashView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+    
+    private func setup() {
+        self.view.addSubview(contentView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false //Para não utilizar o storyboard
+    }
+}
